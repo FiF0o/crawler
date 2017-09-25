e7a5a9bcf4593915e284b81bfd4d87983588081f
Jon Lazarini <jonathan.lazarini@wipro.com>
2017-09-25 10:42:12 +0100

    refactor code to have func

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import sys
 from module import logger
-print(sys.path)
+# print(sys.path)
 
 # Append module to sys path here
 
-# Beginning
-intro = logger('Marv')
-print('... est un vier')
+
+import urllib
+from lxml import html
+
+url = "http://wiprodigital.com/"
+page = html.fromstring(urllib.urlopen(url).read())
+
+def printLink(page):
+    for link in page.xpath("//a"):
+        print "Name", link.text, "URL", link.get("href")
+    return
 
 if __name__ == "__main__":
-    print("running the program")
-    
+    print("running the program...")
+    printLink(page)
